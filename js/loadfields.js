214ae30ad5a08744a04f7ccad55388b67465f59b
m.saravi <mahdi.saravi@gmail.com>
2020-07-13 12:18:11 +0430

    add leaf exclusion behavior to tests

--- js/loadfields.js
+++ js/loadfields.js
@@ -1,7 +1,22 @@
+
+// this file is no longer used and implemented within python app
+exeption_nodes = [] 
 {{Settings}}
+sub_answer = []
+</script>
+{{cloze::AnswerGraph}}
+<script>
+
+sub_answer.forEach(function(element, index) {
+    exeption_nodes.push(...element)
+	pycmd("save_cloze_id_"+index)
+});
+
 json_data = {{TestGraph}};
 graph_data.setData(json_data);
 graph_data.setNotes({{Notes}});
 drawer = chart_tree;
 json = graph_data.stratify();
-refresh_view();
+refresh_view(exeption_nodes);
+
+
